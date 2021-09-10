e44ccca8985d5b9904d45d5d55c5875e04c2954b
Imran Irshad <imran.ib@live.com>
2021-09-10 10:27:50 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000, () => {
+    console.log(`
+🚀 Server ready at: http://localhost:4000/graphql
+⭐️ See sample queries: http://pris.ly/e/ts/graphql-nestjs#using-the-graphql-api
+`);
+  });
+}
+bootstrap();
